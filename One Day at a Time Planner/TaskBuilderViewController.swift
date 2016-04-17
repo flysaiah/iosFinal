7d9675e219873c60a8e69931aaa27f5bad3dc279
flysaiah <mayeis01@yahoo.com>
2016-04-17 01:19:12 -0500

    Hooked up taskBuilder controller, also added SwiftyButton and TextFieldEffects via pods

--- One Day at a Time Planner/TaskBuilderViewController.swift
+++ One Day at a Time Planner/TaskBuilderViewController.swift
@@ -14,8 +14,65 @@ protocol taskBuilderDelegate: class {
 
 class TaskBuilderViewController: UIViewController {
     
-    var delegate: taskBuilderDelegate?
+    struct Task: CustomStringConvertible {
+        var title: String
+        var desc: String?
+        var estimatedTime: Int
+        
+        var description: String {
+            get {
+                return "Task(\(title), \(desc), \(estimatedTime))"
+            }
+        }
+    }
     
+    private struct tbModel: CustomStringConvertible {
+        var tasks: [Task]
+        var freeTime: [(Int, Int)]
+        
+        var description: String {
+            get {
+                return "tbModel(Tasks: \(tasks), freeTime: \(freeTime)"
+            }
+        }
+    }
     
-    @IBOutlet weak var enterTaskTextField: UITextField!
+    private var tbm = tbModel(tasks: [], freeTime: [])
+    
+    var delegate: taskBuilderDelegate? {
+        didSet {
+            tbm.freeTime = delegate!.getFreeTime()
+        }
+    }
+    
+    @IBOutlet weak var enterTaskField: UITextField!
+    @IBOutlet weak var enterDescField: UITextField!
+    
+    // This should eventually be a different UI element
+    @IBOutlet weak var etField: UITextField!
+    
+    @IBAction func addTask(sender: UIButton) {
+        
+        // This needs to be cleaned up later
+        if let titleText = enterTaskField.text {
+            if let est_time = etField.text {
+                if let ET = NSNumberFormatter().numberFromString(est_time) {
+                    let newTask = Task(title: titleText, desc: enterDescField.text, estimatedTime: Int(ET))
+                    tbm.tasks.append(newTask)
+                } else {
+                    print("Error here")
+                }
+            } else {
+                print("ERRRRRRRERRRRR")
+            }
+        } else {
+            print("Error handling should be happening here")
+        }
+        
+        print(String(tbm))
+        
+        // reset fields
+        enterTaskField.text = ""
+        enterDescField.text = ""
+    }
 }
