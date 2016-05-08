77dfdc207a5acba3811949abd031783974417dec
flysaiah <mayeis01@yahoo.com>
2016-05-08 00:39:41 -0500

    Basic drag and drop from schedule table to schedule table and to extras table works

--- One Day at a Time Planner/TaskArrangerViewController.swift
+++ One Day at a Time Planner/TaskArrangerViewController.swift
@@ -139,7 +139,7 @@ class TaskArrangerViewController: UIViewController, UITableViewDelegate, UITable
 
     // MARK: Drag-and-drop functionality
     
-    @objc private func longPressRecognizer(gestureRecognizer: UIGestureRecognizer) {
+    @objc private func dragFromExtrasRecognizer(gestureRecognizer: UIGestureRecognizer) {
         // First, reset backgrounds to natural state--this is necessary when we change it in state.Changed
         resetBackgrounds()
         let longPress = gestureRecognizer as! UILongPressGestureRecognizer
@@ -184,17 +184,25 @@ class TaskArrangerViewController: UIViewController, UITableViewDelegate, UITable
         case UIGestureRecognizerState.Ended:
             // Update cell models, remove snapshot
             let locationInScheduleTable = longPress.locationInView(scheduleTableView)
+            // First check if it's hovering over table cells
             if let ip = scheduleTableView.indexPathForRowAtPoint(locationInScheduleTable) {
                 // add cell to schedule model, remove it from extras model
                 var tmp = scheduleModel![ip.section]
                 tmp.insert(extrasModel[LongPressConstants.initialIndexPath!.row], atIndex: ip.row)
-                scheduleModel![ip.section] = tmp
-                extrasModel.removeAtIndex(LongPressConstants.initialIndexPath!.row)
+                // Only make changes if there's enough time in the block to handle the new task
+                if getRemainingTime(forSection: ip.section, sectionTasks: tmp) >= 0 {
+                    scheduleModel![ip.section] = tmp
+                    extrasModel.removeAtIndex(LongPressConstants.initialIndexPath!.row)
+                }
+                // Default way to add is to drag section header
             } else if let sectionIndex = getTargetedSectionIndex(locationInScheduleTable) {
                 var tmp = scheduleModel![sectionIndex]
                 tmp.append(extrasModel[LongPressConstants.initialIndexPath!.row])
-                scheduleModel![sectionIndex] = tmp
-                extrasModel.removeAtIndex(LongPressConstants.initialIndexPath!.row)
+                // Only make changes if there's enough time in the block to handle the new task
+                if getRemainingTime(forSection: sectionIndex, sectionTasks: tmp) >= 0 {
+                    scheduleModel![sectionIndex] = tmp
+                    extrasModel.removeAtIndex(LongPressConstants.initialIndexPath!.row)
+                }
             }
             LongPressConstants.cellSnapshot?.removeFromSuperview()
             extraTasksTableView.reloadData()
@@ -203,6 +211,90 @@ class TaskArrangerViewController: UIViewController, UITableViewDelegate, UITable
             LongPressConstants.cellSnapshot = nil
         default: break
         }
+    }
+    
+    @objc private func dragFromScheduleRecognizer(gestureRecognizer: UIGestureRecognizer) {
+        // First, reset backgrounds to natural state--this is necessary when we change it in state.Changed
+        resetBackgrounds()
+        let longPress = gestureRecognizer as! UILongPressGestureRecognizer
+        let state = longPress.state
+        let locationInScheduleTable = longPress.locationInView(scheduleTableView)
+        let locationInView = longPress.locationInView(self.view)
+        let indexPath = scheduleTableView.indexPathForRowAtPoint(locationInScheduleTable)
+        
+        // Holds static information about our original cell location, and the "snapshot" we create of the cell to create nice "dragging" UI effect
+        struct LongPressConstants {
+            static var cellSnapshot: UIView? = nil
+            static var initialIndexPath: NSIndexPath? = nil
+        }
+        
+        switch state {
+        case UIGestureRecognizerState.Began:
+            // Create snapshot and hide cell
+            if indexPath != nil {
+                LongPressConstants.initialIndexPath = indexPath
+                let cell = scheduleTableView.cellForRowAtIndexPath(indexPath!) as! TaskScheduleCell!
+                LongPressConstants.cellSnapshot = snapshotOfCell(cell)
+                let center = CGPoint(x: locationInView.x, y: locationInView.y)
+                LongPressConstants.cellSnapshot!.center = center
+                LongPressConstants.cellSnapshot!.transform = CGAffineTransformMakeScale(1.05, 1.05)
+                self.view.addSubview(LongPressConstants.cellSnapshot!)
+                cell.hidden = true
+            }
+        case UIGestureRecognizerState.Changed:
+            // Update snapshot location and highlight schedule table section if hovering over
+            if let snapshot = LongPressConstants.cellSnapshot {
+                let center = CGPoint(x: locationInView.x, y: locationInView.y)
+                snapshot.center = center
+                
+                if let sectionIndex = getTargetedSectionIndex(locationInScheduleTable) {
+                    // Make sure this isn't the original section
+                    if sectionIndex != LongPressConstants.initialIndexPath!.section {
+                        let header = scheduleTableView.headerViewForSection(sectionIndex)
+                        header?.backgroundView?.backgroundColor = UIColor.darkGrayColor()
+                    }
+                }
+            }
+        case UIGestureRecognizerState.Ended:
+            // Update cell models, remove snapshot
+            // First check if it's hovering over table cells
+            if let ip = scheduleTableView.indexPathForRowAtPoint(locationInScheduleTable) {
+                var tmp = scheduleModel![ip.section]
+                tmp.insert(scheduleModel![LongPressConstants.initialIndexPath!.section][LongPressConstants.initialIndexPath!.row], atIndex: ip.row)
+                if ip.section == LongPressConstants.initialIndexPath!.section {
+                    tmp.removeAtIndex(LongPressConstants.initialIndexPath!.row + 1)
+                    scheduleModel![ip.section] = tmp
+                } else {
+                    scheduleModel![ip.section] = tmp
+                    scheduleModel![LongPressConstants.initialIndexPath!.section].removeAtIndex(LongPressConstants.initialIndexPath!.row)
+                }
+
+
+                // Then, check if we're inserting into another header
+            } else if let sectionIndex = getTargetedSectionIndex(locationInScheduleTable) {
+                // Can't re-insert into same section
+                if sectionIndex != LongPressConstants.initialIndexPath!.section {
+                    var tmp = scheduleModel![sectionIndex]
+                    tmp.append(scheduleModel![LongPressConstants.initialIndexPath!.section][LongPressConstants.initialIndexPath!.row])
+                    // Only make changes if there's enough time in the block to handle the new task
+                    if getRemainingTime(forSection: sectionIndex, sectionTasks: tmp) >= 0 {
+                        scheduleModel![sectionIndex] = tmp
+                        scheduleModel![LongPressConstants.initialIndexPath!.section].removeAtIndex(LongPressConstants.initialIndexPath!.row)
+                    }
+                }
+                // Default is to put it back into the extras list
+            } else {
+                extrasModel.append(scheduleModel![LongPressConstants.initialIndexPath!.section][LongPressConstants.initialIndexPath!.row])
+                scheduleModel![LongPressConstants.initialIndexPath!.section].removeAtIndex(LongPressConstants.initialIndexPath!.row)
+            }
+            LongPressConstants.cellSnapshot?.removeFromSuperview()
+            extraTasksTableView.reloadData()
+            scheduleTableView.reloadData()
+            LongPressConstants.initialIndexPath = nil
+            LongPressConstants.cellSnapshot = nil
+        default: break
+        }
+
     }
     
     private func snapshotOfCell(inputView: UIView) -> UIView {
@@ -242,8 +334,11 @@ class TaskArrangerViewController: UIViewController, UITableViewDelegate, UITable
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // Long press to start dragging
-        let longPress = UILongPressGestureRecognizer(target: self, action: #selector(longPressRecognizer))
-        extraTasksTableView.addGestureRecognizer(longPress)
+        // Long press to start dragging from extras table to schedule table
+        let longPress1 = UILongPressGestureRecognizer(target: self, action: #selector(dragFromExtrasRecognizer))
+        extraTasksTableView.addGestureRecognizer(longPress1)
+        // Long press to start dragging from schedule table to either schedule table or extras table
+        let longPress2 = UILongPressGestureRecognizer(target: self, action: #selector(dragFromScheduleRecognizer))
+        scheduleTableView.addGestureRecognizer(longPress2)
     }
 }
